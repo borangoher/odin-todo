669839e62d1710a9fb364d8a70b8c5319fefa02a
borangoher <goherboran1337@gmail.com>
2022-07-23 18:15:49 +0300

    update to 1.0

--- src/buttonLogic.js
+++ src/buttonLogic.js
@@ -1,5 +1,22 @@
+const _elementsFromHtml = function (htmlString) {
+    const template = document.createElement("template");
+    template.innerHTML = htmlString.trim();
+    return template.content;
+}
+
+const _nameGetter = function (elem) {
+    return elem.textContent;
+}
+
 const addProject = function (sidebarElement) {
-    let projectName = prompt("Enter project name.");
+    const projectElements = document.getElementsByClassName("project");
+    const projectNames = Array.from(projectElements).map(_nameGetter);
+    let projectName = "";
+
+    while (projectName === "" || projectNames.includes(projectName)) {
+        projectName = prompt("Enter project name.");
+    }
+
     const newDiv = document.createElement("div");
     newDiv.classList.add("project");
     newDiv.textContent = projectName;
@@ -7,7 +24,63 @@ const addProject = function (sidebarElement) {
 }
 
 const addToDo = function (mainElement) {
-    
+    let toDoName = prompt("Enter to-do name.");
+    let toDoDate = prompt("Enter to-do date.");
+    let toDoDesc = prompt("Enter to-do description.");
+
+    let htmlString = `<div class="to-do">
+                        <p class="to-do-name">${toDoName}</p>
+                        <p class="to-do-date">${toDoDate}</p>
+                    </div>
+                    <div class="expanded-to-do invisible">
+                        <div class="expanded-to-do-line">
+                            <p class="to-do-name">${toDoName}</p>
+                            <p class="to-do-date">${toDoDate}</p>
+                        </div>
+                        <div class="expanded-to-do-line-2"></div>
+                            <p class="to-do-desc">${toDoDesc}</p>
+                            <button>Done</button>
+                    </div>`;
+
+    let elements = _elementsFromHtml(htmlString);
+    mainElement.appendChild(elements.firstElementChild);
+    mainElement.appendChild(elements.lastElementChild);
 }
 
-export {addProject, addToDo};
+const expandToDo = function () {
+    const expandedElements = document.getElementsByClassName("expanded-to-do");
+    let elem;
+    for (elem of expandedElements) {
+        if (!(elem.classList.contains("invisible"))) {
+            elem.classList.add("invisible");
+        }
+    }
+
+    event.currentTarget.nextElementSibling.classList.remove("invisible");
+}
+
+const removeToDo = function () {
+    const doneButton = event.currentTarget;
+    doneButton.parentElement.previousElementSibling.remove();
+    doneButton.parentElement.remove();
+}
+
+const switchProject = function (mainElement) {
+    const currentProject = document.getElementsByClassName("current")[0];
+    localStorage.setItem(currentProject.textContent, mainElement.innerHTML);
+
+    const projectElements = document.getElementsByClassName("project");
+    let elem;
+    for (elem of projectElements) {
+        elem.classList.remove("current");
+    }
+
+    event.currentTarget.classList.add("current");
+    if (localStorage.getItem(event.currentTarget.textContent) == undefined) {
+        mainElement.innerHTML = `<div id="add-to-do">+ To-Do</div>`;
+    } else {
+        mainElement.innerHTML = localStorage.getItem(event.currentTarget.textContent);
+    }
+}
+
+export { addProject, addToDo, expandToDo, removeToDo, switchProject };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,63 @@
-import addProject from "./buttonLogic.js"
+import { addProject, addToDo, expandToDo, removeToDo, switchProject } from "./buttonLogic.js";
+
+const options = { capture : true, };
 
 const addProjectButton = document.getElementById("add-project");
-const sidebarElement = document.getElementById("sidebar")
-addProjectButton.addEventListener("click", function() {addProject(sidebarElement);});
+const sidebarElement = document.getElementById("sidebar");
+const addToDoButton = document.getElementById("add-to-do");
+const mainElement = document.getElementById("main");
+
+addProjectButton.addEventListener("click", function() {addProject(sidebarElement); addProjectSwitches();});
+addToDoButton.addEventListener("click", function () {addToDo(mainElement); addExpandButtons(); addDoneButtons();});
+
+
+const addExpandButtons = function () {
+    let toDoElements = document.getElementsByClassName("to-do");
+    let elem;
+    for (elem of toDoElements) {
+        elem.replaceWith(elem.cloneNode(true));
+    }
+
+    toDoElements = document.getElementsByClassName("to-do");
+    for (elem of toDoElements) {
+        elem.addEventListener("click", function () {expandToDo();}, options);
+    }
+}
+
+const addDoneButtons = function () {
+    let doneButtons = document.getElementsByTagName("button");
+    let btn;
+    for (btn of doneButtons) {
+        btn.replaceWith(btn.cloneNode(true));
+    }
+
+    doneButtons = document.getElementsByTagName("button");
+    for (btn of doneButtons) {
+        btn.addEventListener("click", removeToDo);
+    }
+}
+
+const projectSwitch = function (mainElement) {
+    switchProject(mainElement);
+    let addToDoButtonNew = document.getElementById("add-to-do");
+    addToDoButtonNew.addEventListener("click", function () {addToDo(mainElement); addExpandButtons(); addDoneButtons();});
+    addExpandButtons();
+    addDoneButtons();
+}
+
+const addProjectSwitches = function () {
+    let projectElements = document.getElementsByClassName("project");
+    let elem;
+    for (elem of projectElements) {
+        elem.replaceWith(elem.cloneNode(true));
+    }
+
+    projectElements = document.getElementsByClassName("project");
+    for (elem of projectElements) {
+        elem.addEventListener("click", function () {projectSwitch(mainElement);});
+    }
+}
+
+addExpandButtons();
+addDoneButtons();
+addProjectSwitches();
